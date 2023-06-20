e5f07867c187d38fa44fc210640320eeec358a90
rstreep <127055958+rstreep@users.noreply.github.com>
2023-06-19 23:15:34 -0400

    fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
+// const path = require('path');
+
+// require('dotenv').config({path: path.resolve(__dirname, '../.env')});
+
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize('ecommerce_db', 'root', 'M9d7p6y27!', {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
